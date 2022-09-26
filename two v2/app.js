f50d9eb59a51d209d997fa4b586bcf85915af135
ezrakaba1 <85836377+ezrakaba1@users.noreply.github.com>
2022-09-26 13:25:38 +0200

    2

--- two v2/app.js
+++ two v2/app.js
@@ -1,4 +1,4 @@
-TweenMax.defaultEase = Linear.easeOut;
+/*TweenMax.defaultEase = Linear.easeOut;
 
 new fullpage("#fullpage", {
   //options here
@@ -25,4 +25,19 @@ new fullpage("#fullpage", {
     }
   }
   
+});
+*/
+TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const title = section.querySelector("h1");
+    const tl = new TimelineMax({ delay: 0.5 });
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+  
+  }
 });
